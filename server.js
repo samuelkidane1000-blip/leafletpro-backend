19d93b62ea60c9b0d1cb64e9df154a06a174a1bb
samuelkidane1000-blip <samuelkidane1000@gmail.com>
2026-03-13 17:37:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const nodemailer = require("nodemailer");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
